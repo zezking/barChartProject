cab6d47c5ba1a0f477513619d316f927ed95243d
Enze Zhao <zhaoenze001@gmail.com>
2021-01-29 18:39:10 +0000

    added bar color+spacing cutomization, added title color+fontsize customization

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let gradientGreenBlue = "linear-gradient(to left bottom, #5590e9, #004d7a, #0087
 let gradientBluePink = "linear-gradient(to right, #33ccff 0%, #ff99cc 100%)";
 let gradientYellowGreen = "linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)";
 let defaultBarColor = "#6699CC"
-
+let data1 = [
+  ['Apples', 11],
+  ['Oranges', 24],
+  ['Pears', 18]
+];
 $(function () {
   $("#chart-title").on("input", function () {
     // Print entered value in a div box
@@ -72,20 +76,56 @@ $(document).ready(function () {
       $(".chart-container").empty();
       createBarChart(data1, chart1, options1)
     })
+    // bar customization
+    $(document).ready(function () {
+      $(document).on('click', '#bar', function () {
+        $(".colorPicker").colorPick({
+          'onColorSelected': function () {
+            this.element.css({ 'backgroundColor': this.color, 'color': this.color });
+            console.log($(this).val())
+            options1.barColors = [this.color];
+            createBarChart(data1, chart1, options1)
+          }
+        });
+        $("#SizeSlider").attr({ "min": 0.3, "max": 0.9, "step": 0.1 });
+        $("#SizeSlider").on('input', function () {
+          options1.barSpacing = $(this).val()
+          createBarChart(data1, chart1, options1)
+        })
+      });
+    })
+    //width customization
+    $(document).on('click', '#width', function () {
+      $("#SizeSlider").attr({ "min": 321, "max": 641 });
+
+
+    });
+
+
+
+
+    $(document).on('click', '#title', function () {
+      $(".colorPicker").colorPick({
+        'onColorSelected': function () {
+          this.element.css({ 'backgroundColor': this.color, 'color': this.color });
+          $('h1').css('color', this.color)
+        }
+      });
+      $("#SizeSlider").attr({ "min": 8, "max": 20 })
+      $("#SizeSlider").on("input", function () {
+        $("h1").css("font-size", $(this).val() + "px")
+      })
+    });
+
+    const options1 = {
+      width: 320,
+      height: 320,
+      barSpacing: 0.6,
+      barColors: [defaultBarColor],
+    }
   })
 
   $(".colorPicker").colorPick({ 'initialColor': '#2C3E50' });
-
-  $(document).ready(function () {
-
-    $(".colorPicker").colorPick({
-      'onColorSelected': function () {
-        this.element.css({ 'backgroundColor': this.color, 'color': this.color });
-        $('h1').css('color', this.color)
-      }
-    });
-  })
-
 
   $("#SizeSlider").hide();
   $("#SliderButton").click(function () {
@@ -113,26 +153,21 @@ $(document).ready(function () {
     $(this).parent('li').addClass('current');
   })
 
-  $(document).ready(function () {
-    $(document).on('input', '#SizeSlider', function () {
-      $("h1").css("font-size", $(this).val() + 'px')
-      console.log($(this).val())
-    });
-  })
-
-  $("#dropdownButton").click(function () {
-    $(".dropdown-content").show();
-  })
-
-  $('#dropdownid').val('selectedvalue');
 
 
-
-  const options1 = {
-    width: 320,
-    height: 320,
-    barColors: [defaultBarColor],
-  }
+  $('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+  });
+  $('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+  });
+  $('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+  });
 })
 
 
